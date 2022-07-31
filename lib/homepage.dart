1f5094a7473606d2630ba623294d93f835dadbcc
patelmeet341993 <patelmeet341993@gmail.com>
2022-07-31 12:22:22 +0530

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    print("Build called");
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor:Colors.black,
+        body: Center(
+          child: InkWell(
+            onTap: (){
+              print("Hello click G");
+            },
+            child: Container(
+              //margin: EdgeInsets.only(left: 20,top: 50),
+              padding: EdgeInsets.symmetric(vertical: 10,horizontal: 30),
+              color: Colors.white,
+              child: Text('Hello')
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
+
+
